6ec190a22a287f9ace1a0c18da2b0d8768e0233a
Desislava Peeva <desi.peeva@gmail.com>
2016-05-28 22:09:30 +0300

    Update script, add MQ for < 900px

--- infographic-rotation/js/main.js
+++ infographic-rotation/js/main.js
@@ -1,7 +1,7 @@
 var App = {
   viewport : {
-    w       : $(window).width(),
-    h       : $(window).height()
+    w : $(window).width(),
+    h : $(window).height()
   },
 
   init : function () {
@@ -25,20 +25,24 @@ var App = {
     infographic_container.removeClass('hidden').addClass('animated');
 
     // Infographic Animation
-    var element_size = (App.viewport.w > 1280 ) ? 400 : 350;
+    var element_size = (App.viewport.w > 1280 ) ? 400 : 300;
 
     tl.fromTo( $('#circle'), 1, { width: 0, x: element_size/2, opacity: 0 }, { width: element_size, x: 0, opacity: 1 }, 'start+=0.2' )
       .fromTo( $('#arc-1'), 1, { width: 0, x: element_size/2, rotation: 0, opacity: 0.5 }, { width: element_size, x: 0, rotation: -360, opacity: 1 }, 'start+=0.8' )
       .fromTo( $('#arc-2'), 1, { width: 0, x: element_size/2, rotation: 0 }, { width: element_size, x: 0, rotation: -360 }, 'start+=1.0' )
       .fromTo( $('#arc-3'), 1, { width: 0, x: element_size/2, rotation: 0, opacity: 0 }, { width: element_size, x: 0, rotation: -360, opacity: 1 }, 'start+=1.2' )
-      .to( $('.point-4 .line-cover'), 1, { x: '0' }, 'start+=1.4' )
-      .to( $('.point-4 .title-cover'), 1, { x: '-100%' }, 'start+=1.8' )
-      .to( $('.point-3 .line-cover'), 1, { x: '0' }, 'start+=1.8' )
-      .to( $('.point-3 .title-cover'), 1, { x: '100%' }, 'start+=2.0' )
-      .to( $('.point-2 .line-cover'), 1, { x: '0' }, 'start+=2.0' )
-      .to( $('.point-2 .title-cover'), 1, { x: '-100%' }, 'start+=2.2' )
-      .to( $('.point-1 .line-cover'), 1, { x: '0' }, 'start+=2.2' )
-      .to( $('.point-1 .title-cover'), 1, { x: '100%' }, 'start+=2.4' )
+
+    if ( App.viewport.w > 900 ) {
+      tl.to( $('.point-4 .line-cover'), 1, { x: '0' }, 'start+=1.4' )
+        .to( $('.point-4 .title-cover'), 1, { x: '-100%' }, 'start+=1.8' )
+        .to( $('.point-3 .line-cover'), 1, { x: '0' }, 'start+=1.8' )
+        .to( $('.point-3 .title-cover'), 1, { x: '100%' }, 'start+=2.0' )
+        .to( $('.point-2 .line-cover'), 1, { x: '0' }, 'start+=2.0' )
+        .to( $('.point-2 .title-cover'), 1, { x: '-100%' }, 'start+=2.2' )
+        .to( $('.point-1 .line-cover'), 1, { x: '0' }, 'start+=2.2' )
+        .to( $('.point-1 .title-cover'), 1, { x: '100%' }, 'start+=2.4' )
+    }
+
   }
 
 }
